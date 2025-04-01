70444c74d99f2289f6ced2775287dd92df4aa1bd
Nathaniel Rankine <nathaniel.rankine23@my.stjohns.edu>
2025-03-31 23:16:53 -0400

    finished homework

--- src/main/java/academy/javapro/lab7/AlternativeArithmetic.java
+++ src/main/java/academy/javapro/lab7/AlternativeArithmetic.java
@@ -10,15 +10,17 @@ public class AlternativeArithmetic {
      */
     public static int addWithoutPlus(int a, int b) {
         // Base case: if one of the numbers is 0, return the other
-        // TODO: return a if b is 0
-        // TODO: return b if a is 0
+        if (b == 0)
+            return a;
+        if (a == 0)
+            return b;
 
-        // TODO:Create a while loop that runs until b is 0. This loop will calculate the sum of a and b
-            // TODO: create a variable carry and assign it the result of the bitwise AND operation between a and b
-            // TODO: assign the result of the bitwise XOR operation between a and b to a
-            // TODO: assign the result of the left shift operation on carry by 1 to b
-        // TODO return a because it contains the sum of a and b
-        throw new UnsupportedOperationException("Not implemented yet");
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
     }
 
     /**
@@ -28,30 +30,37 @@ public class AlternativeArithmetic {
      * to use bit shifting for better performance
      */
     public static int divideWithoutDivideOperator(int dividend, int divisor) {
-        // Handle edge cases
-        // TODO: throw an ArithmeticException if divisor is 0
-        // TODO: return 0 if dividend is 0
-        // TODO: return dividend if divisor is 1
+        // Edge cases
+        if (divisor == 0)
+            throw new ArithmeticException("Division by zero");
+        if (dividend == 0)
+            return 0;
+        if (divisor == 1)
+            return dividend;
 
-        // TODO: create a boolean variable isNegative and assign it the result of the XOR operation between dividend and divisor
+        // Determine if the result should be negative
+        boolean isNegative = (dividend < 0) ^ (divisor < 0);
 
         // Convert to positive for the algorithm
-        // TODO: create a long variable absDividend and assign it the absolute value of dividend
-        // TODO: create a long variable absDivisor and assign it the absolute value of divisor
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
 
-        // Use a binary approach for division
-        // TODO: create an int variable result and assign it 0
-        // TODO: while absDividend is greater than or equal to absDivisor
-            // TODO: create a long variable temp and assign it the value of absDivisor
-            // TODO: create a long variable multiple and assign it 1
-            // TODO: while absDividend is greater than or equal to temp shifted left by 1
-                // TODO: shift temp left by 1
-                // TODO: shift multiple left by 1
-            // TODO: subtract temp from absDividend
-            // TODO: add multiple to result using the += operator
+        int result = 0;
+        while (absDividend >= absDivisor) {
+            long temp = absDivisor;
+            long multiple = 1;
+            while (absDividend >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            absDividend -= temp;
+            result += multiple;
+        }
 
-        // TODO: return the result if isNegative is false
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (isNegative) {
+            return -result;
+        }
+        return result;
     }
 
     /**
@@ -69,13 +78,13 @@ public class AlternativeArithmetic {
     public static void main(String[] args) {
         // Test cases for addition
         int[][] additionTests = {
-                {5, 3},     // 8
-                {-2, 7},    // 5
-                {0, 0},     // 0
-                {-5, -3},   // -8
-                {100, 200}, // 300
-                {Integer.MAX_VALUE, 1}, // Edge case: handling overflow
-                {-100, 100} // Additional: adding to zero
+                { 5, 3 }, // 8
+                { -2, 7 }, // 5
+                { 0, 0 }, // 0
+                { -5, -3 }, // -8
+                { 100, 200 }, // 300
+                { Integer.MAX_VALUE, 1 }, // Edge case: handling overflow
+                { -100, 100 } // Additional: adding to zero
         };
 
         System.out.println("Testing addition without '+' operator:");
@@ -88,15 +97,15 @@ public class AlternativeArithmetic {
 
         // Test cases for division
         int[][] divisionTests = {
-                {10, 2},    // 5
-                {15, 3},    // 5
-                {8, 4},     // 2
-                {7, 2},     // 3 (integer division)
-                {100, 10},  // 10
-                {-15, 3},   // -5 (negative dividend)
-                {15, -3},   // -5 (negative divisor)
-                {0, 5},     // 0 (dividend is 0)
-                {1024, 2}   // 512 (power of 2 divisor)
+                { 10, 2 }, // 5
+                { 15, 3 }, // 5
+                { 8, 4 }, // 2
+                { 7, 2 }, // 3 (integer division)
+                { 100, 10 }, // 10
+                { -15, 3 }, // -5 (negative dividend)
+                { 15, -3 }, // -5 (negative divisor)
+                { 0, 5 }, // 0 (dividend is 0)
+                { 1024, 2 } // 512 (power of 2 divisor)
         };
 
         System.out.println("\nTesting division without '/' operator:");
@@ -113,11 +122,11 @@ public class AlternativeArithmetic {
 
         // Test cases for bonus subtraction
         int[][] subtractionTests = {
-                {10, 3},    // 7
-                {5, 8},     // -3
-                {0, 0},     // 0
-                {-5, -3},   // -2
-                {100, 50}   // 50
+                { 10, 3 }, // 7
+                { 5, 8 }, // -3
+                { 0, 0 }, // 0
+                { -5, -3 }, // -2
+                { 100, 50 } // 50
         };
 
         System.out.println("\nTesting subtraction without '-' operator:");
